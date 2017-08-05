22873491b197bae9190b5164039cc92719c15338
Aden Muhammad <mustafamuhammad097@tamu.edu>
2017-08-05 04:04:44 -0500

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({ reset: true });
+
+window.sr = ScrollReveal();
+sr.reveal('p', {duration:1000});
+sr.reveal('h2', {duration:1000});
+sr.reveal('image1', {duration:1000});
+sr.reveal('h1', {duration:1000});
+sr.reveal('h3', {duration:1000});
