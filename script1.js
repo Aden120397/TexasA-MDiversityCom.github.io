64ad9e9734c320098d3d68431a66e934e423d2c6
Aden Muhammad <mustafamuhammad097@tamu.edu>
2017-08-05 10:23:56 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -57,7 +57,7 @@ window.sr = ScrollReveal();
 sr.reveal('p', {duration:1000});
 sr.reveal('h2', {duration:1000});
 sr.reveal('image1', {duration:1000});
-sr.reveal('h1', {duration:1000});
+sr.reveal('h1', {duration:1500});
 sr.reveal('h3', {duration:1000});
 
 
